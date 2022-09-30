adb2bb6d6753c9c7d52f02312ae255631ed04590
Chidubem Ogbudibe <45117618+Dubem101@users.noreply.github.com>
2022-09-30 15:29:53 -0600

    initial commit

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 #  coding: utf-8 
-import socketserver
+import socketserver, os
 
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
 # 
@@ -26,13 +26,110 @@ import socketserver
 
 # try: curl -v -X GET http://127.0.0.1:8080/
 
+WWW_DIR = "www"
+END_OF_LINE_RESPONSE = "\r\n"
+RESPONSE_CODE = {
+    "OK": "200",
+    "REDIRECT": "301",
+    "NOT_FOUND": "404",
+    "METHOD_NOT_ALLOWED": "405"
+}
+
 
 class MyWebServer(socketserver.BaseRequestHandler):
     
     def handle(self):
-        self.data = self.request.recv(1024).strip()
-        print ("Got a request of: %s\n" % self.data)
-        self.request.sendall(bytearray("OK",'utf-8'))
+        self.data = self.request.recv(1024).strip().decode('utf-8')
+
+        #print ("Got a request of: %s\n" % self.data)
+
+        response = ""
+
+        (requestMethod, requestPath) = self.parse_request(self.data)
+
+        if requestMethod != "GET":
+            response = self.get_response(
+                    RESPONSE_CODE["METHOD_NOT_ALLOWED"] + " Method Not Allowed",
+                    self.get_response_header(RESPONSE_CODE["METHOD_NOT_ALLOWED"]),
+                    self.get_content_type(requestPath, RESPONSE_CODE["METHOD_NOT_ALLOWED"]))
+        else:
+            if requestPath.endswith("/"):
+                requestPath = requestPath + "index.html"
+
+            # redirect
+            if self.is_valid_path(requestPath) and self.is_redirect(requestPath):
+                response = self.get_response(
+                        "",
+                        self.get_response_header(RESPONSE_CODE["REDIRECT"]),
+                        "Location: " + requestPath + "/" + END_OF_LINE_RESPONSE)
+            # valid path
+            elif self.is_valid_path(requestPath):
+                try:
+                    file = open(WWW_DIR + requestPath, "r")
+                    fileContent = file.read()
+                    response = self.get_response(
+                        fileContent,
+                        self.get_response_header(RESPONSE_CODE["OK"]),
+                        self.get_content_type(requestPath, RESPONSE_CODE["OK"]))
+                    file.close()
+                except Exception:
+                    response = self.get_response(
+                    RESPONSE_CODE["NOT_FOUND"] + " File Not Found",
+                    self.get_response_header(RESPONSE_CODE["NOT_FOUND"]),
+                    self.get_content_type(requestPath, RESPONSE_CODE["NOT_FOUND"]))
+            # otherwise, not found
+            else:
+                response = self.get_response(
+                    RESPONSE_CODE["NOT_FOUND"] + " Not Found",
+                    self.get_response_header(RESPONSE_CODE["NOT_FOUND"]),
+                    self.get_content_type(requestPath, RESPONSE_CODE["NOT_FOUND"]))
+        
+        #print(response)
+
+        self.request.sendall(bytearray(response, 'utf-8'))
+
+    def parse_request(self, request):
+        requestMethod = request.split(" ")[0]
+        requestPath = request.split(" ")[1]
+        return (requestMethod, requestPath)
+    
+    def is_redirect(self, path):
+        return os.path.isdir(WWW_DIR + path)
+
+    def is_valid_path(self, path):
+        return WWW_DIR in os.path.abspath(WWW_DIR + path)
+
+    def get_response_header(self, responseCode):
+        if responseCode == RESPONSE_CODE["OK"]:
+            return "HTTP/1.1 200 OK" + END_OF_LINE_RESPONSE
+        elif responseCode == RESPONSE_CODE["REDIRECT"]:
+            return "HTTP/1.1 301 Moved Permanently" + END_OF_LINE_RESPONSE
+        elif responseCode == RESPONSE_CODE["NOT_FOUND"]:
+            return "HTTP/1.1 404 Not Found" + END_OF_LINE_RESPONSE
+        elif responseCode == RESPONSE_CODE["METHOD_NOT_ALLOWED"]:
+            return "HTTP/1.1 405 Method Not Allowed" + END_OF_LINE_RESPONSE
+
+    def get_content_type(self, path, responseCode=RESPONSE_CODE["OK"]):
+        contentType = "Content-Type: "
+        if responseCode != RESPONSE_CODE["OK"]:
+            return contentType + "text/plain" + END_OF_LINE_RESPONSE
+
+        if path.endswith(".css"):
+            return contentType + "text/css" + END_OF_LINE_RESPONSE
+        elif path.endswith(".html"):
+            return contentType + "text/html;charset=utf-8" + END_OF_LINE_RESPONSE
+        else:
+            return contentType + "text/plain" + END_OF_LINE_RESPONSE
+
+    def get_content_length(self, content):
+        return "Content-Length: " + str(len(content)) + END_OF_LINE_RESPONSE
+
+    def get_response(self, content, *headerElements):
+        response = ""
+        for element in headerElements:
+            response += element
+        response += END_OF_LINE_RESPONSE
+        return response + content
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
